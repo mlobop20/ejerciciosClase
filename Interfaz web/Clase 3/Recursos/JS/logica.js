fa5914c1d79966dd9ad01dc0e6c08bc57fb7f89d
mlobop20 <maikollp@hotmail.com>
2023-02-11 11:37:32 -0600

    Clase 3

---
+++ Interfaz web/Clase 3/Recursos/JS/logica.js
@@ -0,0 +1,20 @@
+//declaración guardar o reservar un pequeño espacio en la memoria
+// var puede variar su contenido, variable global
+var title;
+//indica variable structural
+let subtitle;
+//constante, espacio en memoria que no cambia valor
+const height = 200; //se inicializa el valor
+title='Este es mi primer ejercicio de JS'
+subtitle = 'Que emoción!'
+
+//string, number, array, boolean
+
+//array
+List = ['manzana', 'peras', 'sandias']
+
+//Objeto es la representación de un elemento del mundo
+var user = {
+    name:'Maikol',
+    lastName: 'Lobo'
+}
